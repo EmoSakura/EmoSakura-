86be519a1fb56742c548a450a9720ef13dd9700e
EmoSakura <120745018+EmoSakura@users.noreply.github.com>
2025-03-07 17:34:57 +0800

    Create scrwipts.js

---
+++ scrwipts.js
@@ -0,0 +1,39 @@
+body {
+    font-family: Arial， sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #4CAF50;
+    color: white;
+    padding: 1em;
+    text-align: center;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+    background-color: #333;
+    overflow: hidden;
+}
+
+nav ul li {
+    float: left;
+}
+
+nav ul li a {
+    color: white;
+    padding: 14px 20px;
+    display: block;
+    text-align: center;
+    text-decoration: none;
+}
+
+nav ul li a:hover {
+    background-color: #111;
+}
+
+section {
+    padding: 20px;
+}
